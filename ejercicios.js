e5cdcf48e1bdd0308100c00debcdeecd18e9f356
mero02 <mero2sp@gmail.com>
2024-08-16 08:31:54 -0300

    otra parte de ejercicios js

--- ejercicios.js
+++ ejercicios.js
@@ -55,3 +55,32 @@ function mostrarElementosLista() {
 mostrarElementosLista();
 
 //Punto 5
+function invertirTexto(){
+    const textoOriginal = document.querySelector('#para-invertir p').innerText;
+    const textoInvertido = textoOriginal.split('').reverse().join('');
+    document.querySelector('#texto-invertido p').innerText = textoInvertido;
+}
+
+document.addEventListener('DOMContentLoaded', invertirTexto);
+
+//Punto 6
+function mostrarNombreJson() {
+    const jsonElement = document.getElementById('objeto_json');
+    const jsonData = JSON.parse(jsonElement.textContent);
+    
+    document.querySelector('#nombre-json').innerHTML = `<h6>${jsonData.nombre}</h6>`;
+}
+
+document.addEventListener('DOMContentLoaded', mostrarNombreJson);
+
+//Punto 7
+function mostrarJSON() {
+    const jsonElement = document.getElementById('objeto_json');
+    const jsonData = JSON.parse(jsonElement.textContent);
+    
+    const jsonString = JSON.stringify(jsonData, null, 2);
+    
+    document.querySelector('#obj-json').textContent = jsonString;
+}
+
+document.addEventListener('DOMContentLoaded', mostrarJSON);
